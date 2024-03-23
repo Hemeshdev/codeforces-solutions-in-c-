9fbc8cea7d0b1aa0b48def44719c0b6141e30dab
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-03-23 21:01:53 +0530

    Create A. Odd Divisor.cpp

---
+++ 900/A. Odd Divisor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() 
+{
+    int n,m;
+    cin>>n>>m;
+    int t=min(n,m);
+    if(t%2==0) cout<<"Malvika"<<endl;
+    else cout<<"Akshat"<<endl;
+}
