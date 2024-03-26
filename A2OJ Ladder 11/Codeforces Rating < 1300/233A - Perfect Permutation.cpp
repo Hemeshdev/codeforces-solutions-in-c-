b97263b3d17f53a7d344a69632e62066564ac6df
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-03-26 19:36:46 +0530

    Create 233A - Perfect Permutation.cpp

---
+++ A2OJ Ladder 11/Codeforces Rating < 1300/233A - Perfect Permutation.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main() {
+    int t;
+    cin>>t;
+    if(t%2!=0 || t==1) cout<<-1<<endl;
+    else{
+      vector<int>temp(t,0);
+      for(int i=0;i<t;i++){
+        temp[i] = t+1;
+      }
+      for(int i=0;i<t;i+=2){
+        swap(temp[i],temp[i+1]);
+      }
+      for(auto i:temp){
+        cout<<i<<" ";
+      }
+    }
+}
