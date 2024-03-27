d34b0c7bfe3e2a8106e4d5d5767353df7075588c
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-03-27 23:22:34 +0530

    Create 227B - Effective Approach.cpp

---
+++ A2OJ Ladder 11/Codeforces Rating < 1300/227B - Effective Approach.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    int n ; cin >> n;
+    map<int, int>mp;
+    for (int i = 1 ; i <= n ; i++){
+        int num; cin >> num;
+        mp[num] = i;
+        
+    }
+    int m; cin >> m;
+    vector<int>q(m);
+    for (int i = 0 ; i < m; i++){
+        cin >>q[i];
+    }
+    long long Vasya = 0, Petya =0 ;
+    for (int i = 0 ; i < m ; i++){
+        Vasya +=mp[q[i]];
+        Petya += n - mp[q[i]]  + 1;
+    }
+    cout << Vasya << " " << Petya;
+    return 0;
+}
