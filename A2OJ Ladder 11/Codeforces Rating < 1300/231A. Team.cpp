e059a68334bb8ae23a530e9ab29b3be96996a354
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-03-27 22:07:16 +0530

    Create 231A. Team.cpp

---
+++ A2OJ Ladder 11/Codeforces Rating < 1300/231A. Team.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+    int count = 0;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a==1 && b==1) || (b==1 && c==1) || (c==1 &&a==1) || (a==1 && b==1 && c==1)){
+            count++;
+        }
+    }
+    cout<<count;
+
+}
