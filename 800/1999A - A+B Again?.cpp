0463ab33aefb11b166d91087a695235d2c2c9ed8
Hemesh <112506925+Hemeshdev@users.noreply.github.com>
2024-12-10 18:12:02 +0530

    Create 1999A - A+B Again?.cpp

---
+++ 800/1999A - A+B Again?.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--){
+      string temp;
+      cin>>temp;
+      cout<<(temp[0]-'0')+(temp[1]-'0')<<endl;
+    }
+    
+}
